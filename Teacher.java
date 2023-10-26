4a6855bac3c081a453f1a40ac5639ec93fb03ce3
Md Jaffar Hussain <146517898+jaffar806@users.noreply.github.com>
2023-10-26 21:04:21 +0530

    Add files via upload

---
+++ Teacher.java
@@ -0,0 +1,9 @@
+package learncodingyouyubechannel;
+
+public class Teacher extends Human {
+    public int getAge() {
+        System.out.println("getAge() method in Teacher");
+        return 21;
+    }
+}
+
